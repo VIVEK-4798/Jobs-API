b5052a5c160280639fc57d5fa7e97621dcebdfd8
VIVEK KUMAR <vivkumar4798@gmail.com>
2024-11-05 22:21:06 +0530

    "error-handler updated"

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -37,7 +37,16 @@ if(company === ' ' || position === ' '){
     res.status(StatusCodes.OK).json({ job })
 }
 const deleteJob = async (req, res) => {
-    res.send("delete job")
+    const {user:{userId}, params:{id:jobId}} = req
+
+    const job = await Job.findByIdAndRemove({
+        _id:jobId,
+        createdBy: userId
+    })
+    if(!job){
+        throw new NotFoundError(`No job with job id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).send()
 }
 
 
